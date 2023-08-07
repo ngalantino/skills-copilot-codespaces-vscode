c8f5dae25b7a79c65bfa0beef30441d324824ddd
ngalantino <109929907+ngalantino@users.noreply.github.com>
2023-08-07 12:37:07 +0000

    Copilot second commit.

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() 
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    var
+}
